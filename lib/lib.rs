fd80eb2ffe36e852a49c5bfbe6a366af474c3990
Stephen Akinyemi <appcypher@outlook.com>
2025-04-11 11:02:39 +0100

    feat: move project from microsandbox (microsandbox/microsandbox#175) - Move project from microsandbox (microsandbox/microsandbox#175)

---
+++ lib/lib.rs
@@ -0,0 +1,8 @@
+//! `did-wk` is a library for working with DID-Web-Key and DID-Key decentralized identifiers (DIDs) methods.
+
+#![warn(missing_docs)]
+#![allow(clippy::module_inception)]
+
+//--------------------------------------------------------------------------------------------------
+// Exports
+//--------------------------------------------------------------------------------------------------
